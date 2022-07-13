492e8b99f525300f567a03a859fd3d2b4d72acdc
naser23 <naserabdulkadir0@gmail.com>
2022-07-13 12:58:38 -0400

    Hamburger menu is functional. Need to work on Desktop navbar and links for both next.

--- src/components/Navbars/MobileNav.js
+++ src/components/Navbars/MobileNav.js
@@ -4,10 +4,12 @@ import "../../styles/mobilenav.css";
 function MobileNav() {
   return (
     <nav className="mobileNav">
-      <div className="link">About</div>
-      <div className="link">Projects</div>
-      <div className="link">Contact</div>
-      <div className="link">Resume</div>
+      <div className="linkArea">
+        <div className="mobileLink">About</div>
+        <div className="mobileLink">Projects</div>
+        <div className="mobileLink">Contact</div>
+        <div className="mobileLink">Resume</div>
+      </div>
     </nav>
   );
 }
