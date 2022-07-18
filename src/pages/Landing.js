1a0e412996420fe00a4293014fd33abce3a0efc1
naser23 <naserabdulkadir0@gmail.com>
2022-07-18 13:22:45 -0400

    Working on entry animation for Landing component.

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,20 +1,26 @@
 import React from "react";
+import { motion } from "framer-motion";
 import "../styles/landing.css";
 
 function Landing() {
   return (
     <section className="landingPage">
-      <div className="landingPageContent">
+      <motion.div
+        className="landingPageContent"
+        initial={{ opacity: 0.5 }}
+        animate={{ opactiy: 1 }}
+        transition={{ duration: 0.2 }}
+      >
         <h1 className="nameHeader">
           Naser <br /> Abdulkadir
         </h1>
-        <h3 className="landingSubHeader">Future Software Developer</h3>
-        <p>
+        <h3 className="landingSubHeader">Software Developer</h3>
+        <p className="landingParagraph">
           Hello! Im an aspiring Web Developer based out of Alexandria, VA. I
           have a passion for web development and I'm looking to show my skills
           and learn as much as I can. Click below to learn more about me!
         </p>
-      </div>
+      </motion.div>
     </section>
   );
 }
